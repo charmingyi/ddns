5feb4c807e50c12633368deea6407734908c2d37
charmingyi <56344221+charmingyi@users.noreply.github.com>
2024-04-26 16:57:41 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/charmingyi/ddns.git
+
+# Change directory to the cloned repository
+cd your-repository
+
+# Execute the script
+./ddns.sh
