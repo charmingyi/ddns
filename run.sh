42eea0d09baaf600c8cce2289957465b9f8c27b0
charmingyi <56344221+charmingyi@users.noreply.github.com>
2024-04-26 17:08:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Execute the script from the cloned repository
+# Check if the 'ddns' directory already exists
+if [ -d "ddns" ]; then
+    echo "Directory 'ddns' already exists."
+else
+    # Clone the repository
+    git clone https://github.com/charmingyi/ddns.git ddns
+fi
+
+# Change directory to the cloned repository
 cd ddns
 
 # Execute the script
